987a27f0d7ca0009644ccde00f1dffa2fc8d302a
Stefan <shodgeskluck@gmail.com>
2022-09-23 13:32:36 -0400

    error handling for gallery saver call

--- lib/helpers/gallery_helper.dart
+++ lib/helpers/gallery_helper.dart
@@ -3,9 +3,10 @@ import 'package:gallery_saver/gallery_saver.dart';
 class GalleryHelper {
   Future<bool?> saveImage(String url) async {
     try {
-      var result = await GallerySaver.saveImage(url);
-    } catch (e) {
-      print(e);
+      await GallerySaver.saveImage(url);
+    } on Error {
+      throw Future.error('Could not save to gallery.');
     }
+    return true;
   }
 }
