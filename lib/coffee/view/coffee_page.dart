b34df6dc50caf2e53dab41bb01a65b2d907ffdb7
Stefan <shodgeskluck@gmail.com>
2022-09-23 13:31:51 -0400

    scaffolding page layout

--- lib/coffee/view/coffee_page.dart
+++ lib/coffee/view/coffee_page.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class CoffeePage extends StatelessWidget {
@@ -5,6 +6,67 @@ class CoffeePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const CoffeeView();
+  }
+}
+
+class CoffeeView extends StatefulWidget {
+  const CoffeeView({Key? key}) : super(key: key);
+
+  @override
+  State<CoffeeView> createState() => _CoffeeViewState();
+}
+
+class _CoffeeViewState extends State<CoffeeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Very Good Coffee!'),
+      ),
+      body: const _CoffeeBody(),
+    );
+  }
+}
+
+class _CoffeeBody extends StatelessWidget {
+  const _CoffeeBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Image.network(
+                    "https://coffee.alexflipnote.dev/KoZr56EQPp8_coffee.jpg"),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                  child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Get Another'),
+              )),
+              Expanded(
+                  child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save Coffee'),
+              )),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
